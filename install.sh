cd9ae1096bfdc6939810845548a1b3b998fcf237
Alexander Kaidalov <kaidalov@fastvps.ru>
2014-03-13 20:01:30 +0400

    add installer YY work code

---
+++ install.sh
@@ -0,0 +1,278 @@
+#!/bin/bash
+#
+# Installation script for FastVPS disk monitoring solution
+#
+
+
+# Required packages
+DEBIAN_DEPS="wget libstdc++5 parted smartmontools liblwp-useragent-determined-perl libnet-https-any-perl libcrypt-ssleay-perl libfile-spec-perl"
+CENTOS_DEPS="wget libstdc++ parted smartmontools perl-Crypt-SSLeay perl-libwww-perl"
+
+# Diag utilities repo
+DIAG_UTILITIES_REPO="https://github.com/FastVPSEestiOu/........"
+# utilities have suffix of ARCH, i.e. arcconf32 or megacli64
+ADAPTEC_UTILITY=arcconf
+LSI_UTILITY=megacli
+
+# Monitoring script URL
+MONITORING_SCRIPT_URL="https://github.com/FastVPSEestiOu/........../monitoring.pl.tgz"
+
+# Monitoring CRON file
+CRON_FILE=/etc/cron.hourly/monitoringfastvps
+
+# Installation path
+INSTALL_TO=/opt/fastvps_monitoring
+
+# smartd config command to run repiodic tests (short/long)
+SMARTD_COMMAND="# smartd.conf by FastVPS
+\n# backup version of distrib file saved to /etc/smartd.conf.dist
+\n
+\n# Discover disks and run short tests every day at 02:00 and long tests every sunday at 03:00
+\nDEVICESCAN -d removable -n standby -s (S/../.././02|L/../../7/03)"
+
+ARCH=
+DISTRIB=
+
+# init.d script of smartd
+SMARTD_INIT_DEBIAN=/etc/init.d/smartmontools
+SMARTD_INIT_CENTOS=/etc/init.d/smartd
+
+#
+# Functions
+#
+
+check_n_install_debian_deps()
+{
+    echo "Installing Debian dependencies..."
+    res=`apt-get update && apt-get install -y $DEBIAN_DEPS`
+    if [ $? -ne 0 ]
+    then
+        echo "Something went wrong while installing dependencies. APT log:"
+        echo $res
+    fi
+    echo "Finished installation of debian dependencies."
+}
+
+check_n_install_centos_deps()
+{
+    echo "Installing CentOS dependencies..."
+    res=`yum install -y $CENTOS_DEPS`
+    if [ $? -ne 0 ]
+    then
+        echo "Something went wrong while installing dependencies. YUM log:"
+        echo $res
+    fi
+    echo "Finished installation of CentOS dependencies."  
+}
+
+check_n_install_diag_tools()
+{
+    lsi_raid=0
+    adaptec_raid=0
+
+    parted_diag=`parted -mls`
+
+    echo "Checking hardware for LSI or Adaptec raids..."
+    if [ -n "`echo $parted_diag | grep -i adaptec`" ]
+    then
+        echo "Found Adaptec raid"
+        adaptec_raid=1
+    fi
+    if [ -n "`echo $parted_diag | grep -i lsi`" ]
+    then
+        echo "Found LSI raid"
+        lsi_raid=1
+    fi
+
+    if [ $adaptec_raid -eq 0 -a $lsi_raid -eq 0 ]
+    then
+        echo "Hardware raid not found"
+        return
+    fi
+
+    echo ""
+
+    if [ $adaptec_raid -eq 1 ]
+    then
+        echo "Installing diag utilities for Adaptec raid..."
+        install_diag_utils $ADAPTEC_UTILITY $ARCH
+        echo "Finished installation of diag utilities for Apactec raid"
+    fi
+
+    echo ""
+
+    if [ $lsi_raid -eq 1 ]
+    then
+        echo "Installing diag utilities for LSI MegaRaid..."
+        install_diag_utils $LSI_UTILITY $ARCH
+        echo "Finished installation of diag utilities for Apactec raid"
+    fi
+}
+
+install_diag_utils()
+{
+    util=$1
+    arch=$2
+
+    if [ -z "$arch" ]
+    then
+        echo "Architecture is unknown - installing both and trying to determine the right one"
+        wget -qN "$DIAG_UTILITIES_REPO""$util"32 -P $INSTALL_TO
+        wget -qN "$DIAG_UTILITIES_REPO""$util"64 -P $INSTALL_TO
+
+        wget -qN "$DIAG_UTILITIES_REPO""$util"32.sha1 -P $INSTALL_TO
+        wget -qN "$DIAG_UTILITIES_REPO""$util"64.sha1 -P $INSTALL_TO
+
+        chmod +x $INSTALL_TO/"$util"*
+
+        res=`"$INSTALL_TO"/"$util"32 -v 2>&1`
+        if [ $? -eq 0 ]
+        then
+            echo "We are on x86. Creating symlink..."
+            ln -sf $INSTALL_TO/"$util"32 $INSTALL_TO/$util
+        fi
+
+        res=`"$INSTALL_TO"/"$util"64 -v 2>&1`
+        if [ $? -eq 0 ]
+        then
+            echo "We are on x86_64. Creating symlink..."
+            ln -sf $INSTALL_TO/"$util"64 $INSTALL_TO/$util
+        fi
+    else
+        echo "Architecture is $arch - installing..."
+        wget -qN "$DIAG_UTILITIES_REPO""$util"$arch -P $INSTALL_TO
+        wget -qN "$DIAG_UTILITIES_REPO""$util"$arch.sha1 -P $INSTALL_TO
+        
+        chmod +x $INSTALL_TO/"$util"*
+        ln -sf $INSTALL_TO/"$util"$ARCH $INSTALL_TO/$util
+    fi
+
+    cd $INSTALL_TO
+    sha1sum --status -c *.sha1
+        
+    if [ $? -ne 0 ]
+    then
+        echo "Wrong SHA1 checksums! Removing installed utilities"
+        rm -f $INSTALL_TO/$util*
+        exit 1
+    else
+        echo "SHA1 checksums is OK"
+        rm -f $INSTALL_TO/$util*.sha1
+        echo "done."
+    fi
+}
+
+
+install_monitoring_script()
+{
+    echo "Installing monitoring.pl into $INSTALL_TO..."
+    wget -qN --no-check-certificate $MONITORING_SCRIPT_URL -P $INSTALL_TO
+    wget -qN --no-check-certificate $MONITORING_SCRIPT_URL".sha1" -P $INSTALL_TO
+
+    monitoring_script=`basename $MONITORING_SCRIPT_URL`
+
+    cd $INSTALL_TO
+    sha1sum --status -c $monitoring_script.sha1
+        
+    if [ $? -ne 0 ]
+    then
+        echo "Wrong SHA1 checksums! Removing monitoring script"
+        rm -f $INSTALL_TO/$monitoring_script*
+        exit 2
+    else
+        echo "SHA1 checksums is OK"
+        rm -f $INSTALL_TO/$monitoring_script*.sha1
+        echo "Extracting monitoring file"
+        tar -C $INSTALL_TO -xzf $INSTALL_TO/$monitoring_script
+        rm -f $INSTALL_TO/$monitoring_script
+        monitoring_script=${monitoring_script%.tgz}
+        echo -n "Installing CRON task... "
+        echo "#!/bin/bash" > $CRON_FILE
+        echo "perl $INSTALL_TO/$monitoring_script --cron" >> $CRON_FILE
+        chmod +x $CRON_FILE
+        echo "done."
+    fi
+}
+
+
+start_smartd_tests()
+{
+    echo -n "Creating config for smartd... "
+
+    # Backup /etc/smartd.conf
+    if [ ! -e /etc/smartd.conf.dist ]
+    then
+        mv /etc/smartd.conf /etc/smartd.conf.dist
+    fi
+
+    echo $SMARTD_COMMAND > /etc/smartd.conf
+    echo "done."
+
+    # restart service
+    case $DISTRIB in
+        debian)
+        $SMARTD_INIT_DEBIAN restart
+        ;;
+
+        centos)
+        $SMARTD_INIT_CENTOS restart
+        ;;
+    esac
+
+    if [ $? -ne 0 ]
+    then
+        echo "smartd failed to start. This may be caused by absence of disks SMART able to monitor."
+        tail /var/log/daemon.log
+    fi
+}
+
+#
+# Start installation procedure
+#
+
+if [ -n "`echo \`uname -a\` | grep -e \"-686\|i686\"`" ]
+then
+    ARCH=32
+fi
+if [ -n "`echo \`uname -a\` | grep -e \"amd64\|x86_64\"`" ]
+then
+    ARCH=64
+fi
+
+if [ -n "`cat /etc/issue | grep -i \"Debian\"`" ]
+then
+    DISTRIB=debian
+fi
+if [ -n "`cat /etc/issue | grep -i \"CentOS\"`" ]
+then
+    DISTRIB=centos
+fi
+
+echo "We are on $DISTRIB $ARCH"
+
+
+# Dependencies installation
+case $DISTRIB in
+    debian)
+    check_n_install_debian_deps
+    ;;
+
+    centos)
+    check_n_install_centos_deps
+    ;;
+
+    *)
+    echo "Can't determine OS. Exiting..."
+    exit 1
+    ;;
+esac
+
+# Diagnostic tools installation
+check_n_install_diag_tools
+
+# Monitoring script installation
+install_monitoring_script
+
+# Periodic smartd tests
+start_smartd_tests
+
---
+++ monitoring.pl
@@ -0,0 +1,279 @@
+#!/usr/bin/perl
+=description
+
+Check System (what ever)
+
+by Alexander Kaidalov <kaidalov@fastvps.ru>
+
+=cut
+
+use strict;
+use warnings;
+use LWP::UserAgent;
+use HTTP::Request::Common qw(GET POST);
+use File::Spec;
+
+use Data::Dumper;
+
+
+#
+# config
+#
+
+# diagnostic utilities
+use constant {
+    ADAPTEC_UTILITY => '/opt/fastvps_monitoring/arcconf',
+    LSI_UTILITY => '/opt/fastvps_monitoring/megacli'
+};
+
+# API
+use constant API_URL => 'https://monitoring.url';
+
+
+#
+# Functions
+#
+
+# Find disks
+sub find_disks
+{
+    # here we'll save disk => ( info, ... )
+    my %disks = ();
+    
+    # tmp variable for storing disk data
+    my $tmp_disk = {};
+    
+    # get parted -lms output
+    open(PARTED, 'parted -lms|') or die "Can't get parted output. Not installed?!";
+    
+    my $first_pass = 1;
+    my $new_disk = 1;
+    LINE: while(<PARTED>)
+    {
+        # skip empty line
+        next LINE if /^\s/;
+    
+        # new disk
+        if(/^BYT;/)
+        {
+            # add previous disk to hash
+            if(!$first_pass)
+            {
+                %{$disks{$tmp_disk->{"disk"}->{"device"}}} = %$tmp_disk;
+            }
+    
+            $new_disk = 1;
+            $first_pass = 0;
+            next LINE;
+        }
+    
+        # get fields
+        my @fields = split(/:/);
+    
+        # device
+        if($new_disk)
+        {
+            $tmp_disk = {};
+    
+            # model
+            $fields[6] =~ s/;\n$//;
+    
+            # add to list
+            $tmp_disk->{"disk"} = {
+                "device" => $fields[0],
+                "size" => $fields[1],
+                "model" => $fields[6],
+            };
+            @{$tmp_disk->{"partitions"}} = ();
+    
+            #
+            # detect type (raid or disk)
+            #
+            my $type = 'disk';
+            
+            # adaptec
+            if($fields[6] =~ m/adaptec .*(\d+)/i)
+            {
+                $type = 'adaptec';
+    
+                # save array number
+                $tmp_disk->{"disk"}{"array_num"} = $1 + 1;
+            }
+    
+            # MD
+            $type = 'md' if $fields[0] =~ m/\/md\d+/;
+    
+            # LSI (3ware)
+            $type = 'lsi' if $fields[6] =~ m/lsi/i;
+    
+            # add type
+            $tmp_disk->{"disk"}{"type"} = $type;
+    
+            $new_disk = 0;
+            next LINE;
+        }
+    
+        # partition
+        if(!$new_disk)
+        {
+            push(@{$tmp_disk->{"partitions"}}, {
+                "number" => $fields[0],
+                "used" => $fields[1],
+                "total" => $fields[2],
+                "free" => $fields[3]
+            });
+        }
+    }
+
+    # add last disk (if defined)
+    if(scalar(keys(%$tmp_disk)))
+    {
+        %{$disks{$tmp_disk->{"disk"}->{"device"}}} = %$tmp_disk;
+    }
+
+    close(PARTED);
+
+    return %disks;
+}
+
+# Check diagnostic utilities availability
+sub check_disk_utilities
+{
+    my (%disks) = @_;
+
+    my $adaptec_needed = 0;
+    my $lsi_needed = 0;
+
+    while((my $key, my $value) = each(%disks))
+    {
+        # Adaptec
+        if(!$adaptec_needed && $value->{"disk"}{type} eq "adaptec")
+        {
+            die "Adaptec utility not found. Please, install Adaptech raid management utility into " . ADAPTEC_UTILITY . "\n" unless -e ADAPTEC_UTILITY;
+            $adaptec_needed = 1;
+        }
+    
+        # LSI
+        if(!$lsi_needed && $value->{"disk"}{type} eq "lsi")
+        {
+            die "not found. Please, install LSI MegaCli raid management utility into " . LSI_UTILITY . " (symlink if needed)\n" unless -e LSI_UTILITY;
+            $lsi_needed = 1;
+        }
+    
+    }
+
+    return ($adaptec_needed, $lsi_needed);
+}
+
+# Run disgnostic utility for each disk
+sub diag_disks
+{
+    my (%disks) = @_;
+
+    while((my $key, my $value) = each(%disks))
+    {
+        my $type = $value->{"disk"}{"type"};
+        my $res = '';
+        my $cmd = '';
+    
+        # adaptec
+        if($type eq "adaptec")
+        {
+            $cmd = ADAPTEC_UTILITY . " getconfig " . $value->{"disk"}{"array_num"} . " ld";
+        }
+
+        # md
+        if($type eq "md")
+        {    
+            $cmd = 'cat /proc/mdstat';
+        }
+
+        # lsi (3ware)
+        # TODO:
+        if($type eq "lsi")
+        {
+            # it may be run with -L<num> for specific logical drive
+            $cmd = LSI_UTILITY . " -LDInfo -Lall -Aall";
+        }
+    
+        # disk
+        if($type eq "disk")
+        {
+            $cmd = "smartctl --all $key";
+        }
+
+        $res = `$cmd` if $cmd;
+        
+        $disks{$key}{"diag"} = $res;
+    }
+
+    return %disks;
+}
+
+# Send disks diag results
+sub send_disks_results
+{
+    my (%disks) = @_;
+
+    foreach(keys(%disks))
+    {
+    	my $disk = $disks{$_};
+        my $diag = $disk->{'diag'};
+    
+        # send results
+        my $status = 'error';
+        $status = 'success' if $diag ne '';
+        
+        my $req = POST(API_URL, [
+            action => "save_data",
+            status => $status,
+            agent_name => 'disks',
+            agent_data => $diag
+        ]);
+
+        # get result
+        my $ua = LWP::UserAgent->new();
+        my $res = $ua->request($req);
+        
+        # TODO: check $res? old data in monitoring system will be notices
+        #       one way or the other...
+
+        return $res->is_success;
+    }
+}
+
+
+#
+# Run checks
+#
+
+# find disks
+my %disks = find_disks;
+
+# check diag utilities
+check_disk_utilities(%disks);
+
+# get diag data
+%disks = diag_disks(%disks);
+
+# send or show results
+if(scalar @ARGV > 0 and $ARGV[0] eq '--cron')
+{
+    if(!send_disks_results(%disks))
+    {
+        print "Failed to send monitoring data to FastVPS";
+        exit(1);
+    }
+}
+else
+{
+    print "This information was gathered and will be sent to FastVPS:\n";
+    print "Disks found: " . (scalar keys %disks) . "\n\n";
+
+    while((my $key, my $value) = each(%disks))
+    {
+        print $key . " is " . $value->{'disk'}->{'type'} . " with " . (scalar @{$value->{'partitions'}}) . " partitions. Diagnostic data:\n";
+        print $value->{'diag'} . "\n\n";
+    }
+}
+
