4eb1fbb3fd58431f1414f5d228f3b75aa89cf18b
dmaksimovfvps <maksimov@fastvps.ru>
2017-11-02 17:55:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -530,7 +530,7 @@ _restart_smartd()
 
     case $os in
         # systemctl on new OS
-        Debian[8-9]|CentOS7|Ubuntu16 )
+        Debian[8-9]|CentOS7|Ubuntu16|Ubuntu17 )
             restart_cmd='systemctl restart smartd.service'
         ;;
         # /etc/init.d/ on sysv|upstart OS
@@ -571,7 +571,7 @@ _enable_smartd_autostart()
 
     case $os in
         # systemctl on new OS
-        Debian[8-9]|CentOS7|Ubuntu16 )
+        Debian[8-9]|CentOS7|Ubuntu16|Ubuntu17 )
             enable_cmd='systemctl enable smartd.service'
         ;;
         # chkconfig on CentOS 6
